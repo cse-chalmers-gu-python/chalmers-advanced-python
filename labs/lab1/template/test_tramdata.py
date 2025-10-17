1ed5fda7f47b5d7a74295e7f24443383b33c1c8a
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-10-17 13:07:15 +0200

    Lab 1: incorporate query tests into main test file. Simplify template README.

--- labs/lab1/template/test_tramdata.py
+++ labs/lab1/template/test_tramdata.py
@@ -7,14 +7,18 @@ class TestTramData(unittest.TestCase):
 
     def setUp(self):
         with open(TRAM_FILE) as trams:
-            tramdict = json.loads(trams.read())
-            self.stopdict = tramdict['stops']
-            self.linedict = tramdict['lines']
+            self.tramdict = json.loads(trams.read())
+            self.stopdict = self.tramdict['stops']
+            self.linedict = self.tramdict['lines']
 
     def test_stops_exist(self):
         stopset = {stop for line in self.linedict for stop in self.linedict[line]}
         for stop in stopset:
             self.assertIn(stop, self.stopdict, msg = stop + ' not in stopdict')
+
+    def test_query_via(self):
+        ans = answer_query(self.tramdict, "via Chalmers")
+        self.assertEqual(ans, ['6', '7', '8', '10', '13'])
 
     # add your own tests here
 
