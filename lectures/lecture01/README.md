c424fac2df9d9e9ab55ca8fbc193c04fef0ec9be
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-11-03 11:25:08 +0100

    Add files for lecture 01

---
+++ lectures/lecture01/README.md
@@ -0,0 +1,3 @@
+# Lecture 01
+
+Nobel Prize Winners from WikiData: <https://w.wiki/3tEM>
---
+++ lectures/lecture01/nobel.py
@@ -0,0 +1,7 @@
+tbl = [
+  ['person', 'name', 'sex', 'award', 'date', 'birthDate'],
+  ['Q462843', 'Tu Youyou', 'female', 'Nobel Prize in Physiology or Medicine', '2015-01-01', '1930-12-30'],
+  ['Q89620738', 'John Michael Jumper', 'male', 'Nobel Prize in Chemistry', '2024-01-01', '1985-01-01'],
+  ['Q1267', 'Dag Hammarskjöld', 'male', 'Nobel Peace Prize', '1961-01-01', '1905-07-29'],
+]
+
