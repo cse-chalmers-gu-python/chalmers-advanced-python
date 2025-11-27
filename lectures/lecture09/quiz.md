14b1f5606036787eb7d7c3cb4a79398a846d0f86
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-11-27 15:13:26 +0100

    Updates to lecture 09 code, quiz

---
+++ lectures/lecture09/quiz.md
@@ -0,0 +1,31 @@
+# Quiz
+
+## Q1: Simple GET
+
+What status code do we get from:
+
+GET https://www.cse.chalmers.se/~aarne/
+
+## Q2: Adding query string
+
+What status code do we get from:
+
+GET https://www.cse.chalmers.se/~aarne/?foo=bar
+
+## Q3: Changing path
+
+What status code do we get from:
+
+GET https://www.cse.chalmers.se/~foo/?aarne=bar
+
+## Q4: Changing method
+
+What status code do we get from:
+
+PATCH https://www.cse.chalmers.se/~aarne/
+
+## Q5: Changing domain
+
+What status code do we get from:
+
+GET https://www.cse.chalmers123.se/~aarne/
--- lectures/lecture09/server.py
+++ lectures/lecture09/server.py
@@ -3,24 +3,30 @@ from urllib.parse import urlparse, parse_qs
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # http://localhost:8000/hello/?name=Mumin
         if self.path.startswith("/hello"):
             purl = urlparse(self.path)
             params = parse_qs(purl.query)
-            resp = f"Hello, {params.get('name', ['person'])[0]}\n"
+            if "name" in params:
+              resp = f"Hello, {params['name'][0]}!\n"
+            else:
+              resp = f"Hello, world!\n"
             self.send_response(200)
             self.end_headers()
             self.wfile.write(resp.encode())
-        elif self.path == "/bye":
+
+        # http://localhost:8000/bye/
+        elif self.path.startswith("/bye"):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"Goodbye")
+
+        # Catch-all
         else:
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b'Not found')
+            self.send_error(404)
 
 if __name__ == "__main__":
     server_address = ("localhost", 8000)
     httpd = HTTPServer(server_address, MyRequestHandler)
     print(f'Listening on {server_address}...')
-    httpd.serve_forever()
+    httpd.serve_forever() # this will never terminate!
